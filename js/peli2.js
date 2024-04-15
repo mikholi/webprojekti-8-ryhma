28d276504660cd7f8008586a7d07deff3dbeb374
mikholi <keranemi@icloud.com>
2024-04-15 16:45:22 +0300

    peli2:sen alkua

---
+++ js/peli2.js
@@ -0,0 +1,18 @@
+const wordsList = [
+    {
+        word: "verbi"
+        hint: "ilmaisee tekemistä ja olemista"
+    },
+    {
+        word: "adjektiivi"
+        hint: "millainen jokin asia on"
+    },
+    {
+        word: "runo"
+        hint: "usein riimitettyä"
+    },
+    {
+        word: "ruotsi"
+        hint: "Suomen toinen virallinen kieli"
+    },
+]
