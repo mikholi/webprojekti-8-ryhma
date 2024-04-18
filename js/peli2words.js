0867e8128dfff09d878fafe26993b9e72c1ea518
mikholi <keranemi@icloud.com>
2024-04-18 11:19:48 +0300

    mikon peli eka versio

---
+++ js/peli2words.js
@@ -0,0 +1,32 @@
+const wordList = [
+    {
+        word: "ruotsi",
+        hint: "Suomen toinen virallinen kieli"
+    },
+
+    {
+        word: "verbi",
+        hint: "ilmaisee tekemistä ja olemista"
+
+    },
+
+    {
+        word: "runo",
+        hint: "kirjoitettu soinnilliseen muotoon"
+    },
+
+    {
+        word: "romaani",
+        hint: "kirjallinen teos"
+    },
+
+    {
+        word: "kalevala",
+        hint: "Suomen kansalliseepos"
+    }
+    
+       
+]
+
+//Jokaisessa tasossa 10 kysymystä, tasoja voisi olla 5, tämän jälkeen tulee high score lista johon peli kysyy 
+//käyttäjänimen//
