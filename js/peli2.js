6f2cb6fd60244820ab7f5e710ab53ab00025980e
mikholi <keranemi@icloud.com>
2024-04-25 17:54:39 +0300

    pelini idean vaihto

--- js/peli2.js
+++ js/peli2.js
@@ -1,103 +1,54 @@
-const inputs = document.querySelector(".inputs")
-hintTag = document.querySelector(".hint span")
-guessLeft = document.querySelector(".guess-left span")
-wrongLetter = document.querySelector(".wrong-letter span")
-resetBtn = document.querySelector(".reset-btn")
-const nextLevelBtn = document.querySelector(".next-level")
-typingInput = document.querySelector(".typing-input")
-
-let currentLevel = 0; 
-let totalGuesses = 10;
-let currentWordIndex = 0; 
-let word, maxGuesses, incorrectLetters = [], correctLetters = [];
-
-function randomWord() {
-    const levelWords = wordList[currentLevel];
-    const ranItem = levelWords[currentWordIndex];
-    word = ranItem.word;
-    maxGuesses = word.length >= 5 ? 8 : 6;
-    correctLetters = [];
-    incorrectLetters = [];
-    hintTag.innerText = ranItem.hint;
-    guessLeft.innerText = totalGuesses;
-    wrongLetter.innerText = incorrectLetters;
-
-    let html = "";
-    for (let i = 0; i < word.length; i++) {
-        html += `<input type="text" disabled>`;
-    }
-    inputs.innerHTML = html;
-}
-
-function startNextLevel() {
-    currentLevel++; 
-    currentWordIndex = 0; 
-    totalGuesses = 10; 
-    nextLevelBtn.style.display = "none"; 
-    resetBtn.style.display = "block"; 
-    randomWord(); 
-}
+const wordText = document.querySelector(".word"),
+hintText = document.querySelector(".hint span"),
+timeText = document.querySelector(".time b"),
+inputField = document.querySelector("input"),
+refreshBtn = document.querySelector(".refresh-word"),
+checkBtn = document.querySelector(".check-word");
 
 
-function checkLevelCompletion() {
-    if (currentWordIndex === 5) { 
-        currentWordIndex = 0; 
-        currentLevel++; 
-        resetBtn.style.display = "none"; 
-        nextLevelBtn.style.display = "block";
-        alert("Onnea! Pääset seuraavalle tasolle!");
-    }
-}
+let correctWord, timer;
 
-
-
-function initGame(e) {
-    let key = e.target.value.toLowerCase();
-    if (key.match(/^[A-Za-z]+$/) && !incorrectLetters.includes(` ${key}`) &&
-        !correctLetters.includes(key)) {
-        if (word.includes(key)) {
-            for (let i = 0; i < word.length; i++) {
-                if (word[i] == key) {
-                    correctLetters += key;
-                    inputs.querySelectorAll("input")[i].value = key;
-                }
-            }
-        } else {
-            totalGuesses--;
-            guessLeft.innerText = totalGuesses;
-            incorrectLetters.push(` ${key}`);
-            wrongLetter.innerText = incorrectLetters;
+const initTimer = maxTime => {
+    clearInterval(timer);
+    timer = setInterval(() => {
+        if(maxTime > 0) {
+            maxTime--;
+            return timeText.innerText = maxTime;
         }
-       
+        alert(`Aika loppui! ${correctWord.toUpperCase()} oli oikea sana!`);
+        initGame();
+    }, 1000);
+}
+
+const initGame = () => {
+    initTimer(30);
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObj.word.split("");
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
     }
-    typingInput.value = "";
-
-    setTimeout(() => {
-        if (correctLetters.length === word.length) {
-            currentWordIndex++; 
-            checkLevelCompletion(); 
-            randomWord(); 
-        } else if (totalGuesses < 1) {
-            alert("Peli loppui! Sinulla ei ole arvauksia jäljellä!");
-            
-        }
-    }, 100);
+    wordText.innerText = wordArray.join("");
+    hintText.innerText = randomObj.hint;
+    correctWord = randomObj.word.toLowerCase();;
+    inputField.value = "";
+    inputField.setAttribute("maxlength", correctWord.length);
 }
 
-function startNextLevel() {
-    currentLevel++; 
-    currentWordIndex = 0; 
-    nextLevelBtn.style.display = "none"; 
-    resetBtn.style.display = "block"; 
-    randomWord(); 
+const gameStartBtn = document.querySelector(".start-game-btn")
+
+const startGame = () => {
+    initGame()
 }
 
+const checkWord = () => {
+    let userWord = inputField.value.toLowerCase();
+    if(!userWord) return alert("Anna sana tarkistettavaksi!");
+    if(userWord !== correctWord) return alert(`Hups! ${userWord} ei ole oikea sana!`);
+    alert(`Onnea! ${correctWord.toUpperCase()} on oikea sana!`);
+    initGame();
+}
 
-resetBtn.addEventListener("click", randomWord);
-typingInput.addEventListener("input", initGame);
-inputs.addEventListener("click", () => typingInput.focus());
-document.addEventListener("keydown", () => typingInput.focus());
-nextLevelBtn.addEventListener("click", startNextLevel);
-
-
-randomWord();
+refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord);
+gameStartBtn.addEventListener("click", startGame)
--- js/peli2words.js
+++ js/peli2words.js
@@ -1,17 +1,63 @@
-const wordList = [
-    [
-        { word: "verbi", hint: "kuvaa tekemistä ja olemista" },
-        { word: "kalevala", hint: "Suomen kansallisteos" },
-        { word: "kivi", hint: "Seitsemän veljeksen kirjoittajan sukunimi" },
-        { word: "adjektiivi", hint: "kuvaa millainen tai minkälainen asia on" },
-        { word: "ruotsi", hint: "Suomen toinen virallinen kieli" }
-        
-    ],
-    [
-        {word: "substantiivi", hint: "ilmaisee asiaa tai esinettä"},
-        {word: "numeraali", hint: "ilmaisee lukua tai määrää"},
-        {word: "vuorosana", hint: "roolihahmon puheenvuoro näytelmässä tai elokuvassa"},
-        
-    ],
+let words = [
+    {
+        word: "kivi",
+        hint: "Suomen kansalliskirjailijan sukunimi"
+    },
+    {
+        word: "kalevala",
+        hint: "Suomen kansalliseepos"
+    },
+    {
+        word: "verbi",
+        hint: "ilmaisee tekemistä ja olemista"
+    },
+    {
+        word: "adjektiivi",
+        hint: "ilmaisee millainen asia on, laatusana"
+    },
+    {
+        word: "ruotsi",
+        hint: "Suomen toinen virallinen kieli"
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "feather",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
     
-];
+]
