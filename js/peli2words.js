d2c1ff90a4814cfeffcb2c762f79d6010bf48c47
mikholi <keranemi@icloud.com>
2024-05-05 17:50:40 +0300

    titlen vaihto omaan peliin

--- js/peli2words.js
+++ js/peli2words.js
@@ -98,10 +98,10 @@ let words = [
     {
         word: "teksti",
         hint: "luettavaksi tarkoitettu kirjoitus"
-    }, //25 sanaa
+    }, 
     {
         word: "aakkoset",
         hint: "a,b,c,d,e,f,g..."
-    },
+    }, //Pelin sanat tulee randomisti tästä luettelosta//
     
 ]
